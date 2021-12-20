375da50b5daf01e6c7c1f5d8847a0e56dae4ce3f
toastythetoaster <toast@isota.ch>
2021-12-20 17:48:54 -0500

    h

---
+++ index.ts
@@ -0,0 +1,25 @@
+
+/* ———————————————————— Copyright (c) 2021 toastythetoaster ————————————————————
+ *
+ * DiscordExperiments Plugin
+ *
+ * ————————————————————————————————————————————————————————————————————————————— */
+
+import { UPlugin } from '@classes';
+import { getByProps } from '@webpack';
+
+class DiscordExperiments extends UPlugin {
+  start(): void {
+    this._setDeveloper(true);
+  }
+  
+  stop(): void {
+    this._setDeveloper(false);
+  }
+
+  _setDeveloper(a: boolean): void {
+    Object.defineProperty(getByProps('isDeveloper'), 'isDeveloper', { get: () => (a ? 1 : 0), set: a => a, configurable: !0 });
+  }
+}
+
+module.exports = DiscordExperiments;
