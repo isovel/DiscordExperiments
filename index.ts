7d61fd0828155312c3f12d3848ec3183911e7927
toastythetoaster <toast@isota.ch>
2022-01-18 05:16:42 -0500

    updated manifest and deleted license file

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 
-/* ————————————————— Copyright (c) 2021-2022 toastythetoaster ——————————————————
+/* ——————— Copyright (c) 2021-2022 toastythetoaster. All rights reserved. ———————
  *
  * DiscordExperiments Plugin
  *
- * ————————————————————————————————————————————————————————————————————————————— */
+ * —————————————————————————————————————————————————————————————————————————————— */
 
 import { UPlugin } from '@classes';
 import { getByProps } from '@webpack';
