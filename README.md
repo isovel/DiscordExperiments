80d4b677fc38b795dd0f5cce3b274182212dd886
toastythetoaster <toast@isota.ch>
2022-09-06 22:40:16 -0400

    Fixed! Requires a theme to work now.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Discord Experiments Plugin
 Enables developer options in the Discord client.
+
+Requires the [Discord Experiments Patcher](https://github.com/toastythetoaster/DiscordExperimentsPatcher) **theme** installed and enabled to work.
--- index.ts
+++ index.ts
@@ -7,8 +7,22 @@
 
 import { UPlugin } from '@classes';
 import { getByProps } from '@webpack';
+import { patches } from '@patcher';
+
+const strings = {
+  success: 'Patched successfully! Enjoy!',
+  error: 'This plugin requires the Discord Experiments Patcher theme to work! Install the theme, enable it, and reload Discord.',
+  prefix: '[DiscordExperiments]',
+}
 
 class DiscordExperiments extends UPlugin {
+  constructor() {
+    super();
+    if (!!patches.find(p => p.before[0]?.caller === 'DiscordExperimentsPatcher')) 
+      console.warn(`${strings.prefix} ${strings.success}`);
+    else throw new Error(`${strings.prefix} ${strings.error}`);
+  }
+
   start(): void {
     this._setDeveloper(true);
   }
