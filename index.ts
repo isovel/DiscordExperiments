488ecdd8d781d22f75679d0fc575d04725e63dd1
toastythetoaster <toast@isota.ch>
2022-09-07 02:03:42 -0400

    Ignore api calls to `entitlements`

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@
  * —————————————————————————————————————————————————————————————————————————————— */
 
 import { UPlugin } from '@classes';
-import { getByProps } from '@webpack';
-import { patches } from '@patcher';
+import { getByProps, APIModule } from '@webpack';
+import { instead, patches, unpatchAll } from '@patcher';
 
 const strings = {
   success: 'Patched successfully! Enjoy!',
@@ -25,14 +25,29 @@ class DiscordExperiments extends UPlugin {
 
   start(): void {
     this._setDeveloper(true);
+    this._patch();
   }
   
   stop(): void {
     this._setDeveloper(false);
+    unpatchAll('DiscordExperiments');
   }
 
   _setDeveloper(a: boolean): void {
     Object.defineProperty(getByProps('isDeveloper'), 'isDeveloper', { get: () => (a ? 1 : 0), set: a => a, configurable: !0 });
+  }
+
+  _patch(): void {
+    instead('DiscordExperiments', APIModule, 'get', (_, args, original) => {
+      const [options] = args;
+      const { url, query } = options;
+      if (url?.includes('/entitlements') && query?.with_sku) {
+        return new Promise((resolve, reject) => {
+          reject({code: 50035, errors: {}, message: 'Invalid Form Body'});
+        });
+      }
+      return original(args[0]);
+    });
   }
 }
 
